9783793e221e0f3d630bf4de129e464ee81cd445
calculator <guest@>
2019-04-26 21:37:20 +0200

    Flash all memory banks

--- bin/flash.sh
+++ bin/flash.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-./esptool.py --port /dev/ttyUSB0 write_flash 0x00000 0x00000.bin 0x10000 0x10000.bin
+./esptool.py --port /dev/ttyUSB0 write_flash 0x00000 0x00000.bin 0x10000 0x10000.bin 0x7c000 esp_init_data_default.bin 0x3fe000 blank.bin
